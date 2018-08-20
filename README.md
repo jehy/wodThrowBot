c70aaffabb0f98b81bbccca1e956bde9f50c7735
Jehy <fate@jehy.ru>
2018-08-20 13:40:35 +0300

    add some tests, refactor, remove message flag

---
+++ README.md
@@ -0,0 +1,22 @@
+# WodThrowBot
+
+[![Build Status](https://travis-ci.org/jehy/wodThrowBot.svg?branch=master)](https://travis-ci.org/jehy/wodThrowBot)
+[![dependencies Status](https://david-dm.org/jehy/wodThrowBot/status.svg)](https://david-dm.org/jehy/wodThrowBot)
+[![devDependencies Status](https://david-dm.org/jehy/wodThrowBot/dev-status.svg)](https://david-dm.org/jehy/wodThrowBot?type=dev)
+[![Coverage Status](https://coveralls.io/repos/github/jehy/wodThrowBot/badge.svg?branch=master)](https://coveralls.io/github/jehy/wodThrowBot?branch=master)
+
+Simple bor for throwing dices in VTM mechanics.
+
+You can find it on telegram as `@WodThrowBot`.
+
+Syntax:
+```
+/roll numberDice [difficulty] [spec] [damage] [action]
+```
+
+Where
+* `numberDice` is a number of dice you wanna roll
+* `difficulty` is a difficulty, lol
+* `spec` if you are using speciality (10 counts as 2x successes)
+* `damage` if you're gonna roll damage (1 does not subtract successes)
+* `action` description of what you're doing
--- test/index.js
+++ test/index.js
@@ -24,69 +24,202 @@ describe('Some simple tests', ()=>{
       const res = utils.throwDices(params);
       const reply = utils.parseResult(res);
       const resStr = utils.resultToStr(reply);
-      // debug(resStr);
     }
   });
 
 
-  it('should be able to process russian input', ()=>{
-    const test = '5х8';
-    const params = utils.parseRequest(test);
-    const res = utils.throwDices(params);
-    const reply = utils.parseResult(res);
-    const resStr = utils.resultToStr(reply);
-    debug(resStr);
-  });
-
-
-  it('should be able to process speciality throws', ()=>{
-    for (let i = 0; i < 10; i++)
-    {
-      const test = '5x8 spec';
+  describe('Parsing request', ()=>{
+    it('should be able to process russian input', ()=>{
+      const test = '5х8';
       const params = utils.parseRequest(test);
+      assert.equal(params.diceNumber, 5);
+      assert.equal(params.difficulty, 8);
+      assert.equal(params.special, false);
+      assert.equal(params.damage, false);
+      assert.equal(params.action, '');
       const res = utils.throwDices(params);
       const reply = utils.parseResult(res);
       const resStr = utils.resultToStr(reply);
+      assert.isNotTrue(resStr.includes('damage'));
+      assert.isNotTrue(resStr.includes('special'));
+      assert.isNotTrue(resStr.includes('action'));
+      debug(test);
       debug(resStr);
-    }
-  });
+    });
 
-  it('should be able to process damage throws', ()=>{
-    for (let i = 0; i < 10; i++)
-    {
-      const test = '5x8 damage';
+    it('should be able to process english input', ()=>{
+      const test = '5x8';
       const params = utils.parseRequest(test);
+      assert.equal(params.diceNumber, 5);
+      assert.equal(params.difficulty, 8);
+      assert.equal(params.special, false);
+      assert.equal(params.damage, false);
+      assert.equal(params.action, '');
       const res = utils.throwDices(params);
       const reply = utils.parseResult(res);
       const resStr = utils.resultToStr(reply);
+      assert.isNotTrue(resStr.includes('damage'));
+      assert.isNotTrue(resStr.includes('special'));
+      assert.isNotTrue(resStr.includes('action'));
+      debug(test);
       debug(resStr);
-    }
-  });
+    });
 
-  it('should be able to process speciality and damage throws', ()=>{
-    for (let i = 0; i < 10; i++)
-    {
-      const test = '5x8 spec damage';
+    it('should be able to process input without "x"', ()=>{
+      const test = '5 8';
       const params = utils.parseRequest(test);
+      assert.equal(params.diceNumber, 5);
+      assert.equal(params.difficulty, 8);
+      assert.equal(params.special, false);
+      assert.equal(params.damage, false);
+      assert.equal(params.action, '');
       const res = utils.throwDices(params);
       const reply = utils.parseResult(res);
       const resStr = utils.resultToStr(reply);
+      assert.isNotTrue(resStr.includes('damage'));
+      assert.isNotTrue(resStr.includes('special'));
+      assert.isNotTrue(resStr.includes('action'));
+      debug(test);
       debug(resStr);
-    }
-  });
+    });
 
-  it('should be able to pass message', ()=>{
-    for (let i = 0; i < 10; i++)
-    {
-      const test = '5 8 spec damage message Blah-blah blah!';
+
+    it('should be able to process speciality throws', ()=>{
+      const test = '5 8 spec';
+      const test2 = '5 8 s';
+      const test3 = '5 8 ы';
       const params = utils.parseRequest(test);
+      const params2 = utils.parseRequest(test2);
+      const params3 = utils.parseRequest(test3);
+      assert.deepEqual(params, params2);
+      assert.deepEqual(params, params3);
+      assert.equal(params.diceNumber, 5);
+      assert.equal(params.difficulty, 8);
+      assert.equal(params.special, true);
+      assert.equal(params.damage, false);
+      assert.equal(params.action, '');
       const res = utils.throwDices(params);
       const reply = utils.parseResult(res);
       const resStr = utils.resultToStr(reply);
-      assert.length(res.values, 5);
-      assert.isTrue(resStr.indexOf('Blah-blah blah!') > -1);
+      assert.isNotTrue(resStr.includes('damage'));
+      assert.isTrue(resStr.includes('special'));
+      assert.isNotTrue(resStr.includes('action'));
+      debug(test);
       debug(resStr);
-    }
+    });
+
+    it('should be able to process damage throws', ()=>{
+      const test = '5 8 damage';
+      const test2 = '5 8 dmg';
+      const test3 = '5 8 d';
+      const test4 = '5 8 в';
+      const params = utils.parseRequest(test);
+      const params2 = utils.parseRequest(test2);
+      const params3 = utils.parseRequest(test3);
+      const params4 = utils.parseRequest(test4);
+      assert.deepEqual(params, params2);
+      assert.deepEqual(params, params3);
+      assert.deepEqual(params, params4);
+      assert.equal(params.diceNumber, 5);
+      assert.equal(params.difficulty, 8);
+      assert.equal(params.damage, true);
+      assert.equal(params.special, false);
+      assert.equal(params.action, '');
+      const res = utils.throwDices(params);
+      const reply = utils.parseResult(res);
+      const resStr = utils.resultToStr(reply);
+      assert.isTrue(resStr.includes('damage'));
+      assert.isNotTrue(resStr.includes('special'));
+      assert.isNotTrue(resStr.includes('action'));
+      debug(test);
+      debug(resStr);
+    });
+
+    it('should be able to process both speciality and damage throws', ()=>{
+      const test = '5 8 damage spec';
+      const test2 = '5 8 spec dmg';
+      const test3 = '5 8 d s';
+      const test4 = '5 8 ы в';
+      const params = utils.parseRequest(test);
+      const params2 = utils.parseRequest(test2);
+      const params3 = utils.parseRequest(test3);
+      const params4 = utils.parseRequest(test4);
+      assert.deepEqual(params, params2);
+      assert.deepEqual(params, params3);
+      assert.deepEqual(params, params4);
+      assert.equal(params.diceNumber, 5);
+      assert.equal(params.difficulty, 8);
+      assert.equal(params.damage, true);
+      assert.equal(params.special, true);
+      assert.equal(params.action, '');
+      const res = utils.throwDices(params);
+      const reply = utils.parseResult(res);
+      const resStr = utils.resultToStr(reply);
+      assert.isTrue(resStr.includes('damage'));
+      assert.isTrue(resStr.includes('special'));
+      assert.isNotTrue(resStr.includes('action'));
+      debug(test);
+      debug(resStr);
+    });
+
+    describe('passing message', ()=>{
+
+      it('should be able to pass message (no flags, no difficulty)', ()=>{
+        const test = '5 Blah-blah blah!';
+        const params = utils.parseRequest(test);
+        assert.equal(params.diceNumber, 5);
+        assert.equal(params.difficulty, 6);
+        assert.equal(params.damage, false);
+        assert.equal(params.special, false);
+        assert.equal(params.action, 'Blah-blah blah!');
+        const res = utils.throwDices(params);
+        const reply = utils.parseResult(res);
+        const resStr = utils.resultToStr(reply);
+        assert.isNotTrue(resStr.includes('damage'));
+        assert.isNotTrue(resStr.includes('special'));
+        assert.isNotTrue(resStr.includes('Action: Blah-blah blah!'));
+        debug(test);
+        debug(resStr);
+      });
+
+
+      it('should be able to pass message (no flags, difficulty)', ()=>{
+        const test = '5 8 Blah-blah blah!';
+        const params = utils.parseRequest(test);
+        assert.equal(params.diceNumber, 5);
+        assert.equal(params.difficulty, 8);
+        assert.equal(params.damage, false);
+        assert.equal(params.special, false);
+        assert.equal(params.action, 'Blah-blah blah!');
+        const res = utils.throwDices(params);
+        const reply = utils.parseResult(res);
+        const resStr = utils.resultToStr(reply);
+        assert.isNotTrue(resStr.includes('damage'));
+        assert.isNotTrue(resStr.includes('special'));
+        assert.isNotTrue(resStr.includes('Action: Blah-blah blah!'));
+        debug(test);
+        debug(resStr);
+      });
+
+
+      it('should be able to pass (all flags)', ()=>{
+        const test = '5 8 spec damage Blah-blah blah!';
+        const params = utils.parseRequest(test);
+        assert.equal(params.diceNumber, 5);
+        assert.equal(params.difficulty, 8);
+        assert.equal(params.damage, true);
+        assert.equal(params.special, true);
+        assert.equal(params.action, 'Blah-blah blah!');
+        const res = utils.throwDices(params);
+        const reply = utils.parseResult(res);
+        const resStr = utils.resultToStr(reply);
+        assert.isTrue(resStr.includes('damage'));
+        assert.isTrue(resStr.includes('special'));
+        assert.isNotTrue(resStr.includes('Action: Blah-blah blah!'));
+        debug(test);
+        debug(resStr);
+      });
+    });
   });
 
 });
