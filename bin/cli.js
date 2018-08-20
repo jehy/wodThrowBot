8e7478c5214a4b8bab128770734a61cbba2c854e
Jehy <fate@jehy.ru>
2018-08-20 16:02:20 +0300

    add cli

---
+++ bin/cli.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const debug = require('debug')('throwBot');
+const utils = require('../utils');
+
+const msg = process.argv.slice(2).join(' ').trim();
+debug.enabled = true;
+debug(`message: ${msg}`);
+let params;
+try {
+  params = utils.parseRequest(msg);
+}
+catch (e) {
+  debug(`ERROR: ${e.toString()}`);
+  return;
+}
+const res = utils.throwDices(params);
+const reply = utils.parseResult(res);
+const resStr = utils.resultToStr(reply);
+debug(resStr);
