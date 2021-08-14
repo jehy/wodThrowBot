82770cc7044f3b364a21ac146190f3a9322f83d0
Jehy <fate@jehy.ru>
2021-08-14 19:17:23 +0300

    add equal throw test

--- test/throw.js
+++ test/throw.js
@@ -13,6 +13,24 @@ describe('Some simple tests', () => {
     }
   });
 
+  it('should be able to throw many dice equally', () => {
+
+    const randMap = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+    };
+    for (let i = 0; i < 100 * 1000; i++) {
+      const res = randomDice(6);
+      randMap[res]++;
+    }
+    // eslint-disable-next-line no-console
+    console.log(randMap);
+  });
+
   describe('should be able to process some inputs', () => {
     const tests = ['5x8', '3d6x6', '3d6 sum', '3d6 sum', '5d6x4 sum damage spec'];
     tests.forEach((test)=>{
